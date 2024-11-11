be66d255f49e8b9b4120c2c0a1f9710b481203dc
OrangeVinyl <9ou5oo@gmail.com>
2024-11-11 11:03:55 +0900

    chore: move files to dir

--- crawlers/investing_crawl.py
+++ crawlers/investing_crawl.py
@@ -1,12 +1,13 @@
+import os
 import time
 import json
+import datetime
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import concurrent.futures
 
 def set_chrome_driver(headless=True):
     options = webdriver.ChromeOptions()
@@ -48,18 +49,29 @@ def get_article_content(driver, link):
 
 
 def get_news_articles(company_name):
+    print(f"[{datetime.datetime.now()}] Investing.com 기사 수집을 시작합니다.")
+
+    cnt = 0
     results = []
     base_url = f"https://www.investing.com/search/?q={company_name}&tab=news"
     driver = set_chrome_driver(True)
     driver.get(base_url)
 
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".articleItem"))
-    )
+    try:
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".articleItem"))
+        )
+    except Exception as e:
+        print(f"초기 로딩 중 에러 발생: {e}")
+        driver.quit()
+        return []
 
     scroll_down(driver)
 
     article_items = driver.find_elements(By.CSS_SELECTOR, ".articleItem")
+    total_articles = len(article_items)
+
+    print(f"총 {total_articles}개의 기사를 발견했습니다.")
 
     for index, item in enumerate(article_items):
         try:
@@ -70,34 +82,57 @@ def get_news_articles(company_name):
             description_elements = item.find_elements(By.CSS_SELECTOR, ".textDiv p.js-news-item-content")
             description = description_elements[0].text if description_elements else "Description not available"
             date_elements = item.find_elements(By.CSS_SELECTOR, ".date")
-            pDate = date_elements[0].text if date_elements else "Date not available"
+            publish_date = date_elements[0].text if date_elements else "Date not available"
 
-            if "Date not available" in pDate or "Description not available" in description:
+            if "Date not available" in publish_date or "Description not available" in description:
                 continue
 
-            results.append({
-                "title": title,
-                "description": description.strip(),
-                "org_link": link,
-                "pDate": pDate
-            })
+            cnt += 1
 
+            results.append({
+                'cnt': cnt,
+                'title': title,
+                'description': description.strip(),
+                'link': link,
+                'publish_date': publish_date
+            })
 
         except Exception as e:
             print(f"Error processing article {index}: {e}")
 
 ## 다른 방법 찾아보기 - 속도가 너무 느리다.
     # for article in results:
-    #     article['content'] = get_article_content(driver, article['org_link'])
+    #     article['content'] = get_article_content(driver, article['link'])
 
     driver.quit()
 
-    with open("%s_investing_articles.json" % company_name, "w", encoding="utf-8") as outfile:
-        retJson = json.dumps(results, indent=4, sort_keys=True, ensure_ascii=False)
-        outfile.write(retJson)
+    print(f"총 {cnt}개의 기사를 성공적으로 수집하였습니다.")
 
     return results
 
-def main(company_name):
-    get_news_articles(company_name)
-    print("검색이 완료되었습니다.")
+def run(company_name):
+    articles = get_news_articles(company_name)
+
+    if not articles:
+        print("수집된 기사가 없습니다.")
+        return
+
+    # 저장 경로 설정
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    save_dir = os.path.join(base_dir, 'data', 'investing_articles')
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    save_path = os.path.join(save_dir, f'{company_name}_investing_articles.json')
+
+    # 결과 저장
+    with open(save_path, "w", encoding="utf-8") as outfile:
+        res = json.dumps(articles, indent=4, sort_keys=True, ensure_ascii=False)
+        outfile.write(res)
+
+    print("================[총 검색 결과]================")
+    print(f"가져온 데이터 : {len(articles)} 건")
+    print('%s_naver_articles.json SAVED' % company_name)
+    print(f'{save_path} SAVED')
+
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
-from crawlers import naver_news_crawl, investing_crawl
+from crawlers import naver_crawl, investing_crawl
+from summarizer import summarizer
 
 def main():
+    global input_dir
+
     source = input('기업 분류를 선택하세요 (국내/해외): ')
     company_name = input("기업 이름을 입력하세요 : ")
 
     if source == '국내':
-        naver_news_crawl.run(company_name)
+        print("네이버 뉴스를 크롤링합니다...")
+        naver_crawl.run(company_name)
         input_dir = os.path.join("data", "naver_articles")
 
     elif source == '해외':
-        return
+        print("Investing 뉴스를 크롤링합니다...")
+        investing_crawl.run(company_name)
+        input_dir = os.path.join("data", "investing_articles")
+
+    print("기사를 요약합니다...")
+    output_dir = os.path.join("data", "processed_articles")
+    summarizer.run(input_dir, output_dir, company_name, 'naver')
 
 
 if  __name__ == '__main__':
