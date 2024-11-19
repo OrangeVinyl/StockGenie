c1c7c5d41431435a9857e64d3458d3bb6b9d0f0a
OrangeVinyl <9ou5oo@gmail.com>
2024-11-19 17:31:11 +0900

    chore: add return_all_scores=True to pipeline

--- sentiment/en_sentiment.py
+++ sentiment/en_sentiment.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 # pipe = pipeline("text-classification", model="ProsusAI/finbert")
-pipe = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+pipe = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis", return_all_scores=True)
 
 text = "The company's stock price increased by 5% after the announcement."
 
